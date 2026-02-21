643cf20f29edff0bb449c7e3daa06e181625a170
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2026-02-21 17:08:46 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module jppgo
+
+go 1.24.0
+
+require (
+	github.com/TylerBrock/colorjson v0.0.0-20200706003622-8a50f05110d2 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
+	github.com/tidwall/gjson v1.18.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/TylerBrock/colorjson"
+	flag "github.com/spf13/pflag"
+	"github.com/tidwall/gjson"
+)
+
+func main() {
+	indent := flag.IntP("indent", "i", 4, "indent level")
+	maxDepth := flag.IntP("max-depth", "d", -1, "maximum depth (-1 = unlimited)")
+	path := flag.StringP("path", "p", "", "gjson path (e.g. hits.0.analytics)")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s [options] [file]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "Examples:\n")
+		fmt.Fprintf(os.Stderr, "  cat file.json | %s\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s data.json\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if *indent < 0 {
+		fmt.Fprintln(os.Stderr, "Indent must be non-negative.")
+		os.Exit(1)
+	}
+
+	var input []byte
+	args := flag.Args()
+
+	if len(args) > 1 {
+		fmt.Fprintln(os.Stderr, "Only one input file may be specified.")
+		os.Exit(1)
+	}
+
+	if len(args) == 1 {
+		fileBytes, err := os.ReadFile(args[0])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading file:", err)
+			os.Exit(1)
+		}
+		input = fileBytes
+	} else {
+		stat, _ := os.Stdin.Stat()
+		if (stat.Mode() & os.ModeCharDevice) != 0 {
+			fmt.Fprintln(os.Stderr, "No input provided. Pipe JSON or specify a file.")
+			os.Exit(1)
+		}
+
+		stdinBytes, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			panic(err)
+		}
+		input = stdinBytes
+	}
+
+	if *path != "" {
+		result := gjson.GetBytes(input, *path)
+		if !result.Exists() {
+			fmt.Fprintln(os.Stderr, "Path not found.")
+			os.Exit(1)
+		}
+		input = []byte(result.Raw)
+	}
+
+	var data interface{}
+	if err := json.Unmarshal(input, &data); err != nil {
+		panic(err)
+	}
+
+	if *maxDepth >= 0 {
+		data = truncateDepth(data, 0, *maxDepth)
+	}
+
+	formatter := colorjson.NewFormatter()
+	formatter.Indent = *indent
+
+	coloredJSON, err := formatter.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+
+	os.Stdout.Write(coloredJSON)
+}
+
+func truncateDepth(v interface{}, current, max int) interface{} {
+	if current >= max {
+		switch v.(type) {
+		case map[string]interface{}:
+			return "{...}"
+		case []interface{}:
+			return "[...]"
+		default:
+			return v
+		}
+	}
+
+	switch val := v.(type) {
+	case map[string]interface{}:
+		newMap := make(map[string]interface{})
+		for k, v2 := range val {
+			newMap[k] = truncateDepth(v2, current+1, max)
+		}
+		return newMap
+
+	case []interface{}:
+		newArr := make([]interface{}, len(val))
+		for i, v2 := range val {
+			newArr[i] = truncateDepth(v2, current+1, max)
+		}
+		return newArr
+
+	default:
+		return val
+	}
+}
